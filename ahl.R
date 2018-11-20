34932ec368093d2123a035a5d4c1c663d1cd7fba
Peder Landsverk <pglandsverk@gmail.com>
2018-11-20 15:55:29 +0100

    upd

--- ahl.R
+++ ahl.R
@@ -3,33 +3,25 @@ scriptpath <- ComfyInTurns::myPath()
 devnull <- file('/dev/null','w')
 sink(devnull,type = 'message')
 
-library(reticulate)
+library(stringdist)
 
 library(stringr)
 library(glue)
 library(magrittr)
+library(dplyr)
 
 library(jsonlite)
 library(redux)
 
+library(parallel)
+
 sink(type = 'message')
+close(devnull)
 
-# Placeholders #####################
+# Parallel stuff ###################
 
-reticulateVenv <- 'dfi'
-
-# Reticulate stuff #################
-
-if(!reticulateVenv %in% virtualenv_list()){
-	warning('creating virtualenv')
-	virtualenv_create(reticulateVenv)
-	}
-
-fw <- tryCatch(import('fuzzywuzzy'), error = function(x){
-	warning('installing dependency: fuzzywuzzy')
-	virtualenv_install(reticulateVenv,'fuzzywuzzy')
-	import('fuzzywuzzy')
-	})
+ncores <- detectCores() - 1
+cluster <- makeCluster(ncores,type = 'FORK')
 
 # Read config from stdin (dfi) #####
 
@@ -40,56 +32,32 @@ config <- readLines('stdin')%>%
 
 col <- config$`field to search`
 lookupf <- config$`path to lookupfile`
-threshold <- config$`fuzz threshold`%>%
-	as.numeric()
+fuzzfunc <- config$`fuzz function`
 
 key <- config$redis$listkey
 chunksize <- config$`chunksize`
 
 # Sort out configuration ###########
 
-fuzzfunc <- fw$fuzz$ratio
-
-lookup <- read.delim(lookupf)[1]%>%
-	unlist()
+lookup <- file(lookupf)%>%
+	readLines()
 
 # Redis stuff ######################
 
 redis <- hiredis()
-
-# The function #####################
-# If string in df$col is too similar
-# to any strings in lookupf, remove
-# the row. #########################
-
-fuzzKeep <- function(df,col,lookup,threshold,fuzzfunc){
-	results <- sapply(df[[col]],function(string){
-		sapply(lookup,fuzzfunc,string)%>%
-			max()
-		})
-	keep <- which(results > threshold)
-	sapply(keep,function(x){
-		writeLines(paste(x,results[x]))
-		writeLines(as.character(df[x,col]))
-		})
-
-
-	df[keep,]
-	}
 
 # Process data #####################
 
 startTime <- Sys.time()
 DBgratia::redisChunkApply(redis,
                           key,
-                          FUN = fuzzKeep, 
-			  col = col,
-			  lookup = lookup,
-			  threshold = threshold,
-			  fuzzfunc = fuzzfunc,
+                          FUN = AgnusGlareTool::fuzzScore, 
+                          col = col,
+                          lookup = lookup,
                           chunksize = chunksize,
                           verbose = TRUE)
 endTime <- Sys.time()
+
 
 timeTaken <- endTime - startTime
 writeLines(paste('AHL time elapsed:',round(timeTaken,digits = 2),'seconds'))
